8c6f1cc6698dfb0a449adfe600e56c475d1f117d
126180088 <42297731+126180088@users.noreply.github.com>
2021-04-19 11:12:03 +0800

    NvwaFactory
    简单的工厂模式

---
+++ NvwaFactory/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace NvwaFactory
+{
+    class Program
+    {
+        static void Main()
+        {
+            IPerson person;
+            person = Factory.GetPerson("M");
+            person = Factory.GetPerson("W");
+            person = Factory.GetPerson("R");
+            Console.Read();
+        }
+    }
+}
