110c43ca3681578dba46358baa181ac7fedeac6d
126180088 <42297731+126180088@users.noreply.github.com>
2021-04-24 19:35:14 +0800

    桥接模式数据库数据类型转换

---
+++ TypeForBridge/Bridge/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Bridge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //桥接模式使用
+            ADataBank aDataBank = new MySql();//里式代换原则
+
+            //给对象注入关联对象（具体实例）
+            IFileType fileType = new TXT();
+
+            aDataBank.SetFileType(fileType);
+
+            //执行数据转换操作
+            string filePath = aDataBank.Convert();
+
+            Console.WriteLine(filePath);
+
+            Console.Read();
+        }
+    }
+}
